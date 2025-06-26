2398d65604e9b774ec25c84906b4a2b5dc3cd651
Danail Gabenski <dgabenski@fortressinfosec.com>
2025-06-25 18:40:12 -0600

    Updated for Meteor 3.x
    Updated dependencies and peerlibrary ones to sylido ones
    Updated version to use the sylido fork and version is bumped to 0.4.0

--- package.js
+++ package.js
@@ -1,55 +1,55 @@
 Package.describe({
-  name: 'peerlibrary:data-lookup',
-  summary: "Reactively lookup a field in the object",
-  version: '0.3.0',
-  git: 'https://github.com/peerlibrary/meteor-data-lookup.git'
+  name    : "sylido:data-lookup",
+  summary : "Reactively lookup a field in the object",
+  version : "0.4.0",
+  git     : "https://github.com/sylido/meteor-data-lookup.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.4.1',
-    'ecmascript',
-    'underscore',
-    'tracker'
+    "coffeescript@2.4.1",
+    "ecmascript",
+    "underscore@1.6.4",
+    "tracker@1.3.4"
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:computed-field@0.10.0'
+    "sylido:computed-field@0.11.0"
   ]);
 
-  api.export('DataLookup');
+  api.export("DataLookup");
 
-  api.mainModule('lib.coffee');
+  api.mainModule("lib.coffee");
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom('METEOR@1.8.1');
+  api.versionsFrom("METEOR@3.2.2");
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.4.1',
-    'ecmascript',
-    'random',
-    'underscore',
-    'reactive-var'
+    "coffeescript@2.4.1",
+    "ecmascript",
+    "random",
+    "underscore",
+    "reactive-var"
   ]);
 
   // Internal dependencies.
   api.use([
-    'peerlibrary:data-lookup'
+    "sylido:data-lookup"
   ]);
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:classy-test@0.4.0',
-    'peerlibrary:server-autorun@0.8.0'
+    "sylido:classy-test@0.4.0",
+    "sylido:server-autorun@0.8.0"
   ]);
 
   api.addFiles([
-    'tests.coffee'
+    "tests.coffee"
   ]);
 });
